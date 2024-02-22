73900f505d861357ed0aee998b564b07a6e3a4d6
1saifu <saifukhan@hotmail.se>
2024-02-22 20:03:10 +0100

    task 2,3,12

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import prompt from "prompt-sync";
+import { productsModel, offersModel, suppliersModel, ordersModel } from "./create-database.js"
 
 const user = prompt();
 
@@ -27,6 +28,67 @@ while(runApp){
 let input = user("Choose a number between 1-15: ")
 if(input == 1){
     
+}
+
+else if(input == 2){
+
+    const productName = user("Enter a product name: ");
+
+    const allCategories = await productsModel.distinct("category"); //Distinct excluderar andra object utom category
+    console.log("All categories:")
+    console.log(allCategories);
+
+    const productCategory = user("Enter an existing category or add a new category: ");
+
+    if(!allCategories.includes(productCategory)){
+        const addNewCategory = user(`"${productCategory}" does not exist. Would you like to add it? (yes/no): `);
+
+        if(addNewCategory.toLowerCase() == 'yes'){
+            console.log(`the category "${productCategory}" has been added`);
+        }
+        else{
+            console.log("Product has not been added. Choose and existing category or add a new category");
+        }
+    }
+
+    const productPrice = parseInt(user("Enter the price for the product: "));
+    const productCost = parseInt(user("Enter the product cost: "));
+    const productStock = parseInt(user("Enter the stock amount for the product: "));
+
+    const newProduct = new productsModel({
+        name: productName,
+        category: productCategory,
+        price: productPrice,
+        cost: productCost,
+        stock: productStock,
+    })
+
+    await newProduct.save();
+    console.log("New product has been added!")
+
+}
+
+else if(input == 3){
+
+    const allCategories = await productsModel.distinct("category"); //Distinct excluderar andra object utom category
+    console.log("All categories:")
+    console.log(allCategories);
+
+    const inputCategory = user("Enter a category: ")
+
+    const viewProductByCategory = await productsModel.find({ category: inputCategory });
+
+    console.log("Products in choosen category:")
+    console.log(viewProductByCategory);
+
+}
+
+else if(input == 12){
+
+    const allSuppliers = await suppliersModel.find({}); 
+    console.log("All suppliers:")
+    console.log(allSuppliers);
+
 }
 
 else if(input == 15){
