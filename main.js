cffbe5fd8bc207cc1ec3e6ad27cafe2ca9c8177f
1saifu <saifukhan@hotmail.se>
2024-02-25 00:26:17 +0100

    Did over task 1-5

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import prompt from "prompt-sync";
-import { productsModel, offersModel, suppliersModel, ordersModel } from "./create-database.js"
+import { productsModel, offersModel, suppliersModel, ordersModel, categoriesModel } from "./create-database.js"
 
 const user = prompt();
 
@@ -28,17 +28,19 @@ while(runApp){
 let input = user("Choose a number between 1-15: ")
 if(input == 1){
 
-    const allCategories = await productsModel.distinct("category"); //Distinct excluderar andra object utom category
+    const allCategories = await categoriesModel.distinct("name"); 
     console.log("All existing categories:")
     console.log(allCategories);
     
-    const newCategory = user("Add a new category: ");
+    const myNewCategory = user("Add a new category: ");
 
-    const newProductCategory = new productsModel({
-        category: newCategory
+    const newCategory = new categoriesModel({
+        name: myNewCategory
     });
 
-    await newProductCategory.save();
+    await newCategory.save();
+
+    console.log("A new category has been added!");
 
 }
 
@@ -46,55 +48,114 @@ else if(input == 2){
 
     const productName = user("Enter a product name: ");
 
-    const allCategories = await productsModel.distinct("category"); 
+    const allCategories = await categoriesModel.distinct("name"); 
     console.log("All existing categories:")
     console.log(allCategories);
 
     const productCategory = user("Enter an existing category or add a new category: ");
 
+    const categoryObject = await categoriesModel.findOne({ name: productCategory });
+
     if(!allCategories.includes(productCategory)){
         const addNewCategory = user(`"${productCategory}" does not exist. Would you like to add it? (yes/no): `);
 
-        if(addNewCategory.toLowerCase() == 'yes'){
+        if(addNewCategory.toLowerCase() === 'yes'){
+            const newCategory = new categoriesModel({
+                name: productCategory
+            })
+            categoryObject = await newCategory.save();
             console.log(`the category "${productCategory}" has been added`);
         }
         else{
             console.log("Product has not been added. Choose and existing category or add a new category");
         }
     }
+    
+    const allSuppliers = await suppliersModel.find({}); 
+    console.log("All suppliers:")
+    console.log(allSuppliers);
+    
+    const supplierName = user("Enter the supplier name: ");
+    
+    let supplier= await suppliersModel.findOne({
+        name: supplierName,
+        category: categoryObject._id
+    })
 
-    const supplier = await suppliersModel.find({ name: productCategory });
+if(!supplier){
+
+    const supplierContact = user("Enter the supplier contact: ");
+    const supplierEmail = user("Enter the supplier email: ");
+
+    const addNewSupplier = user("Supplier not found. Would you like to add a new supplier? (yes/no)");
+
+    if(addNewSupplier.toLocaleLowerCase() === "yes"){
+        supplier  = await suppliersModel.create({
+        name: supplierName,
+        contact: supplierContact,
+        email: supplierEmail,
+        category: categoryObject._id,
+        })
+        console.log(`${supplierName} has been added!`)
+    } else{
+        console.log("Choose an existing supplier or add a new one")
+    }
+}
 
     //parseInt converts string to number
     const productPrice = parseInt(user("Enter the price for the product: "));
     const productCost = parseInt(user("Enter the product cost: "));
     const productStock = parseInt(user("Enter the stock amount for the product: "));
 
-    if(supplier){
-    const newProduct = new productsModel({
+  
+    const newProduct = await productsModel.create({
         name: productName,
-        category: productCategory,
+        category: categoryObject._id,
         price: productPrice,
         cost: productCost,
         stock: productStock,
         supplier: supplier._id,
     })
 
-    await newProduct.save();
-    console.log("New product has been added!")
-}
+    console.log(newProduct, "has been added!")
 
 }
 
 else if(input == 3){
 
-    const allCategories = await productsModel.distinct("category"); 
+    const allCategories = await categoriesModel.distinct("name"); 
     console.log("All existing categories:")
     console.log(allCategories);
 
     const inputCategory = user("Enter a category: ")
 
-    const viewProductByCategory = await productsModel.find({ category: inputCategory });
+    const viewProductByCategory = await productsModel.aggregate([
+        {
+            $match: {
+                category: {
+                    $in: await categoriesModel.find({ name: inputCategory }).distinct("_id")
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "suppliers",
+                localField: "supplier",
+                foreignField: "_id",
+                as: "suppliersInfo"
+            }
+        },
+        {
+            $project: {
+                name: 1,
+                price: 1,
+                cost: 1,
+                stock: 1,
+                supplier: "$suppliersInfo.contact"
+            }
+        }
+
+    ]);
 
     console.log("Products in choosen category:")
     console.log(viewProductByCategory);
